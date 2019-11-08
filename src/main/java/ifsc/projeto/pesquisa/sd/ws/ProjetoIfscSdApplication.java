e528263d62f76a2b62cefd2daa2b1bc84f587a32
wilso.fcj <wilson.fcj@aluno.ifsc.edu.br>
2019-11-08 18:05:24 -0200

    Primeiro commit

---
+++ src/main/java/ifsc/projeto/pesquisa/sd/ws/ProjetoIfscSdApplication.java
@@ -0,0 +1,13 @@
+package ifsc.projeto.pesquisa.sd.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIfscSdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIfscSdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ifsc/projeto/pesquisa/sd/ws/projetoifscsd/ProjetoIfscSdApplicationTests.java
@@ -0,0 +1,13 @@
+package ifsc.projeto.pesquisa.sd.ws.projetoifscsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIfscSdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
