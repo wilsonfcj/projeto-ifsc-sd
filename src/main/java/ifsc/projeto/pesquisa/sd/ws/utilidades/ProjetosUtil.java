c8a5eaf06ade3ec520748e2d38ec099772cfdcdc
wilsonfcj <wilson.fcj@aluno.ifsc.edu.br>
2019-11-30 12:20:27 -0200

    Corrigindo emails para poder dar split

--- src/main/java/ifsc/projeto/pesquisa/sd/ws/utilidades/ProjetosUtil.java
+++ src/main/java/ifsc/projeto/pesquisa/sd/ws/utilidades/ProjetosUtil.java
@@ -21,7 +21,7 @@ public class ProjetosUtil {
 				"Desenvolvimento de um aplicativo para o ensino e aprendizagem e produtos notáveis e polinômios");
 		lProjeto.setTipo("Projeto Interno por Editais (Projeto Novo)");
 		lProjeto.setCategoria("Pesquisa aplicada");
-		lProjeto.setSituacao(Situacao.EM_EXECUÇÃO.codigo);
+		lProjeto.setSituacao(Situacao.EM_EXECUCAO.codigo);
 		lProjeto.setArea("COORDENADORIA DO CURSO DE CIÊNCIAS DA COMPUTAÇÃO (CAMPUS LAGES) (11.01.21.02.17)");
 		lProjeto.setEmailCoordenador("ailton.durigon@ifsc.edu.br");
 		lProjeto.setCoordenador("AILTON DURIGON");
@@ -45,7 +45,7 @@ public class ProjetosUtil {
 		lProjeto3.setTitulo("Guia de qualidade para provedores de serviços web");
 		lProjeto3.setTipo("Projeto Interno por Editais (Projeto Novo)");
 		lProjeto3.setCategoria("Pesquisa aplicada");
-		lProjeto3.setSituacao(Situacao.EM_EXECUÇÃO.codigo);
+		lProjeto3.setSituacao(Situacao.EM_EXECUCAO.codigo);
 		lProjeto3.setArea("COORDENADORIA DO CURSO DE CIÊNCIAS DA COMPUTAÇÃO (CAMPUS LAGES) (11.01.21.02.17)");
 		lProjeto3.setEmailCoordenador("alexandre.perin@ifsc.edu.br");
 		lProjeto3.setCoordenador("ALEXANDRE PERIN DE SOUZA");
@@ -57,7 +57,7 @@ public class ProjetosUtil {
 		lProjeto4.setTitulo("Demandas de Aprendizagem de Inglês no IFSC-câmpus Lages");
 		lProjeto4.setTipo("Projeto Interno por Editais (Projeto Novo)");
 		lProjeto4.setCategoria("Pesquisa aplicada");
-		lProjeto4.setSituacao(Situacao.EM_EXECUÇÃO.codigo);
+		lProjeto4.setSituacao(Situacao.EM_EXECUCAO.codigo);
 		lProjeto4
 				.setArea("COORDENADORIA DO CURSO TECNICO EM INFORMATICA PARA INTERNET (CAMPUS LAGES) (11.01.21.02.20)");
 		lProjeto4.setEmailCoordenador("luciane.oliveira@ifsc.edu.br");
@@ -71,9 +71,9 @@ public class ProjetosUtil {
 				"Utilização do planejamento Simplex-Centróide para o desenvolvimento de polímeros biodegradáveis para armazenamento de alimentos: otimização das propriedades físico-químicas e antioxidantes.");
 		lProjeto5.setTipo("Projeto Interno por Editais (Projeto Novo)");
 		lProjeto5.setCategoria("Pesquisa aplicada");
-		lProjeto5.setSituacao(Situacao.EM_EXECUÇÃO.codigo);
+		lProjeto5.setSituacao(Situacao.EM_EXECUCAO.codigo);
 		lProjeto5.setArea("COORDENADORIA DE CURSOS TÉCNICOS DE BIOTECNOLOGIA (CAMPUS LAGES) (11.01.21.02.01)");
-		lProjeto5.setEmailCoordenador("ana.veeck@ifsc.edu.br|anaveeck@yahoo.com.br");
+		lProjeto5.setEmailCoordenador("ana.veeck@ifsc.edu.br | anaveeck@yahoo.com.br");
 		lProjeto5.setCoordenador("ANA PAULA DE LIMA VEECK");
 		lProjeto5.setDataCadastro(DateUtil.criarData(16, 3, 2019));
 		listProjetos.add(lProjeto5);
@@ -84,9 +84,9 @@ public class ProjetosUtil {
 				"Síntese verde de nanopartículas de prata para aplicação em biopolímeros: desenvolvimento de uma embalagem bioativa, biodegradável e inteligente para a conservação de alimentos.");
 		lProjeto6.setTipo("Projeto Interno por Editais (Projeto Novo)");
 		lProjeto6.setCategoria("Pesquisa aplicada");
-		lProjeto6.setSituacao(Situacao.EM_EXECUÇÃO.codigo);
+		lProjeto6.setSituacao(Situacao.EM_EXECUCAO.codigo);
 		lProjeto6.setArea("COORDENADORIA DE CURSOS TÉCNICOS DE BIOTECNOLOGIA (CAMPUS LAGES) (11.01.21.02.01)");
-		lProjeto6.setEmailCoordenador("ana.veeck@ifsc.edu.br|anaveeck@yahoo.com.br");
+		lProjeto6.setEmailCoordenador("ana.veeck@ifsc.edu.br | anaveeck@yahoo.com.br");
 		lProjeto6.setCoordenador("ANA PAULA DE LIMA VEECK");
 		lProjeto6.setDataCadastro(DateUtil.criarData(9, 4, 2019));
 		listProjetos.add(lProjeto6);
@@ -122,7 +122,7 @@ public class ProjetosUtil {
 		lProjeto9.setCategoria("Pesquisa aplicada");
 		lProjeto9.setSituacao(Situacao.SUBMETIDO.codigo);
 		lProjeto9.setArea("COORDENADORIA DOS CURSOS TÉCNICOS DE ELETROMECÂNICA (CAMPUS LAGES) (11.01.21.02.07)");
-		lProjeto9.setEmailCoordenador("ariton.araldi@ifsc.edu.br|ariton.araldi@gmail.com");
+		lProjeto9.setEmailCoordenador("ariton.araldi@ifsc.edu.br | ariton.araldi@gmail.com");
 		lProjeto9.setCoordenador("ARITON ARALDI");
 		lProjeto9.setDataCadastro(DateUtil.criarData(12, 11, 2019));
 		listProjetos.add(lProjeto9);
@@ -133,10 +133,10 @@ public class ProjetosUtil {
 				"Métodos Computacionais baseados em Inteligência Artificial para a Predição de Risco de Evasão de Alunos no Instituto Federal de Santa Catarina.");
 		lProjeto10.setTipo("Projeto Interno por Editais (Projeto Novo)");
 		lProjeto10.setCategoria("Pesquisa aplicada");
-		lProjeto10.setSituacao(Situacao.EM_EXECUÇÃO.codigo);
+		lProjeto10.setSituacao(Situacao.EM_EXECUCAO.codigo);
 		lProjeto10
 				.setArea("COORDENADORIA DO CURSO TECNICO EM INFORMATICA PARA INTERNET (CAMPUS LAGES) (11.01.21.02.20)");
-		lProjeto10.setEmailCoordenador("andres.ferrero@ifsc.edu.br|anfer86@gmail.com");
+		lProjeto10.setEmailCoordenador("andres.ferrero@ifsc.edu.br | anfer86@gmail.com");
 		lProjeto10.setCoordenador("CARLOS ANDRES FERRERO");
 		lProjeto10.setDataCadastro(DateUtil.criarData(9, 4, 2019));
 		listProjetos.add(lProjeto10);
@@ -149,7 +149,7 @@ public class ProjetosUtil {
 		lProjeto11.setCategoria("Pesquisa aplicada");
 		lProjeto11.setSituacao(Situacao.SUBMETIDO.codigo);
 		lProjeto11.setArea("COORDENADORIA DO CURSO SUPERIOR EM ENGENHARIA MECÂNICA (CAMPUS LAGES) (11.01.21.02.18)");
-		lProjeto11.setEmailCoordenador("eliana.borragini@ifsc.edu.br|efborragini@gmail.com");
+		lProjeto11.setEmailCoordenador("eliana.borragini@ifsc.edu.br | efborragini@gmail.com");
 		lProjeto11.setCoordenador("ELIANA FERNANDES BORRAGINI");
 		lProjeto11.setDataCadastro(DateUtil.criarData(13, 11, 2019));
 		listProjetos.add(lProjeto11);
@@ -160,7 +160,7 @@ public class ProjetosUtil {
 				"DEFINIÇÃO DE CURVA ÓTIMA PARA GEOMETRIA DE PROPULSOR A JATO DE BOCAL DIVERGENTE CONVERGENTE COM COMBUSTÍVEIS GASOSOS.");
 		lProjeto12.setTipo("Projeto Interno por Editais (Projeto Novo)");
 		lProjeto12.setCategoria("Pesquisa aplicada");
-		lProjeto12.setSituacao(Situacao.EM_EXECUÇÃO.codigo);
+		lProjeto12.setSituacao(Situacao.EM_EXECUCAO.codigo);
 		lProjeto12.setArea("COORDENADORIA DO CURSO SUPERIOR EM ENGENHARIA MECÂNICA (CAMPUS LAGES) (11.01.21.02.18)");
 		lProjeto12.setEmailCoordenador("fernando.osorio@ifsc.edu.br");
 		lProjeto12.setCoordenador("FERNANDO DA SILVA OSÓRIO");
@@ -173,7 +173,7 @@ public class ProjetosUtil {
 				"CONSTRUÇÃO DE BANCADA DIDÁTICA MÁQUINAS DE FLUXO, TURBOMÁQUINAS E TÉCNICAS DE MEDIÇÃO DE VAZÃO PARA FLUIDOS.");
 		lProjeto13.setTipo("Projeto Interno por Editais (Projeto Novo)");
 		lProjeto13.setCategoria("Pesquisa aplicada");
-		lProjeto13.setSituacao(Situacao.EM_EXECUÇÃO.codigo);
+		lProjeto13.setSituacao(Situacao.EM_EXECUCAO.codigo);
 		lProjeto13.setArea("COORDENADORIA DO CURSO SUPERIOR EM ENGENHARIA MECÂNICA (CAMPUS LAGES) (11.01.21.02.18)");
 		lProjeto13.setEmailCoordenador("fernando.osorio@ifsc.edu.br");
 		lProjeto13.setCoordenador("FERNANDO DA SILVA OSÓRIO");
@@ -188,7 +188,7 @@ public class ProjetosUtil {
 		lProjeto14.setCategoria("Desenvolvimento tecnológico e inovação");
 		lProjeto14.setSituacao(Situacao.SUBMETIDO.codigo);
 		lProjeto14.setArea("COORDENADORIA DOS CURSOS TÉCNICOS DE ELETROMECÂNICA (CAMPUS LAGES) (11.01.21.02.07)");
-		lProjeto14.setEmailCoordenador("gabriel.madruga@ifsc.edu.b|ggmadruga@gmail.com");
+		lProjeto14.setEmailCoordenador("gabriel.madruga@ifsc.edu.b | ggmadruga@gmail.com");
 		lProjeto14.setCoordenador("GABRIEL GRANZOTTO MADRUGA");
 		lProjeto14.setDataCadastro(DateUtil.criarData(24, 10, 2019));
 		listProjetos.add(lProjeto14);
@@ -198,7 +198,7 @@ public class ProjetosUtil {
 		lProjeto15.setTitulo("Atividade fotocatalítica de TiO2 dopado com mangânes e imobilizado em carvão ativado");
 		lProjeto15.setTipo("Projeto Interno por Editais (Projeto Novo)");
 		lProjeto15.setCategoria("Pesquisa aplicada");
-		lProjeto15.setSituacao(Situacao.EM_EXECUÇÃO.codigo);
+		lProjeto15.setSituacao(Situacao.EM_EXECUCAO.codigo);
 		lProjeto15.setArea(
 				"OORDENADORIA DO CURSO SUPERIOR DE TECNOLOGIA EM PROCESSOS QUÍMICOS (CAMPUS LAGES) (11.01.21.02.16)");
 		lProjeto15.setEmailCoordenador("jaqueline.suave@ifsc.edu.br");
@@ -212,10 +212,10 @@ public class ProjetosUtil {
 				"Avaliação do efeito crioprotetor de hidrolisados proteicos do colágeno na conservação da qualidade de pães de queijo congelados");
 		lProjeto16.setTipo("Projeto Interno por Editais (Projeto Novo)");
 		lProjeto16.setCategoria("Pesquisa aplicada");
-		lProjeto16.setSituacao(Situacao.EM_EXECUÇÃO.codigo);
+		lProjeto16.setSituacao(Situacao.EM_EXECUCAO.codigo);
 		lProjeto16.setArea(
 				"OORDENADORIA DO CURSO SUPERIOR DE TECNOLOGIA EM PROCESSOS QUÍMICOS (CAMPUS LAGES) (11.01.21.02.16)");
-		lProjeto16.setEmailCoordenador("joao.provesi@ifsc.edu.br|provesijg@yahoo.com.br");
+		lProjeto16.setEmailCoordenador("joao.provesi@ifsc.edu.br | provesijg@yahoo.com.br");
 		lProjeto16.setCoordenador("JOAO GUSTAVO PROVESI");
 		lProjeto16.setDataCadastro(DateUtil.criarData(11, 3, 2019));
 		listProjetos.add(lProjeto16);
@@ -225,9 +225,9 @@ public class ProjetosUtil {
 		lProjeto17.setTitulo("Desenvolvimento de sorvete probiótico utilizando a microencapsulação e hidrolisados do colágeno como crioprotetores microbianos");
 		lProjeto17.setTipo("Projeto Interno por Editais (Projeto Novo)");
 		lProjeto17.setCategoria("Pesquisa aplicada");
-		lProjeto17.setSituacao(Situacao.EM_EXECUÇÃO.codigo);
+		lProjeto17.setSituacao(Situacao.EM_EXECUCAO.codigo);
 		lProjeto17.setArea("COORDENADORIA DO CURSO SUPERIOR DE TECNOLOGIA EM PROCESSOS QUÍMICOS (CAMPUS LAGES) (11.01.21.02.16)");
-		lProjeto17.setEmailCoordenador("joao.provesi@ifsc.edu.br|provesijg@yahoo.com.br");
+		lProjeto17.setEmailCoordenador("joao.provesi@ifsc.edu.br | provesijg@yahoo.com.br");
 		lProjeto17.setCoordenador("JOAO GUSTAVO PROVESI");
 		lProjeto17.setDataCadastro(DateUtil.criarData(29, 3, 2019));
 		listProjetos.add(lProjeto17);
@@ -239,7 +239,7 @@ public class ProjetosUtil {
 		lProjeto18.setCategoria("Pesquisa aplicada");
 		lProjeto18.setSituacao(Situacao.SUBMETIDO.codigo);
 		lProjeto18.setArea("COORDENADORIA DO CURSO SUPERIOR DE TECNOLOGIA EM PROCESSOS QUÍMICOS (CAMPUS LAGES) (11.01.21.02.16)");
-		lProjeto18.setEmailCoordenador("joao.provesi@ifsc.edu.br|provesijg@yahoo.com.br");
+		lProjeto18.setEmailCoordenador("joao.provesi@ifsc.edu.br | provesijg@yahoo.com.br");
 		lProjeto18.setCoordenador("JOAO GUSTAVO PROVESI");
 		lProjeto18.setDataCadastro(DateUtil.criarData(24, 10, 2019));
 		listProjetos.add(lProjeto18);
@@ -251,7 +251,7 @@ public class ProjetosUtil {
 		lProjeto19.setCategoria("Pesquisa aplicada");
 		lProjeto19.setSituacao(Situacao.SUBMETIDO.codigo);
 		lProjeto19.setArea("COORDENADORIA DO CURSO SUPERIOR DE TECNOLOGIA EM PROCESSOS QUÍMICOS (CAMPUS LAGES) (11.01.21.02.16)");
-		lProjeto19.setEmailCoordenador("joao.provesi@ifsc.edu.br|provesijg@yahoo.com.br");
+		lProjeto19.setEmailCoordenador("joao.provesi@ifsc.edu.br | provesijg@yahoo.com.br");
 		lProjeto19.setCoordenador("JOAO GUSTAVO PROVESI");
 		lProjeto19.setDataCadastro(DateUtil.criarData(31, 10, 2019));
 		listProjetos.add(lProjeto19);
@@ -261,9 +261,9 @@ public class ProjetosUtil {
 		lProjeto20.setTitulo("Aplicação de biopolímeros antioxidantes a base de amido de pinhão e pectina cítrica na conservação pós-colheita de uvas.");
 		lProjeto20.setTipo("Projeto Interno por Editais (Projeto Novo)");
 		lProjeto20.setCategoria("Pesquisa aplicada");
-		lProjeto20.setSituacao(Situacao.EM_EXECUÇÃO.codigo);
+		lProjeto20.setSituacao(Situacao.EM_EXECUCAO.codigo);
 		lProjeto20.setArea("COORDENADORIA CURSOS TECNICO EM AGROECOLOGIA (CAMPUS LAGES) (11.01.21.02.10)");
-		lProjeto20.setEmailCoordenador("jocleita.ferrareze@ifsc.edu.br|joferrareze@yahoo.com.br");
+		lProjeto20.setEmailCoordenador("jocleita.ferrareze@ifsc.edu.br | joferrareze@yahoo.com.br");
 		lProjeto20.setCoordenador("JOCLEITA PERUZZO FERRAREZE");
 		lProjeto20.setDataCadastro(DateUtil.criarData(10, 4, 2019));
 		listProjetos.add(lProjeto20);
@@ -275,7 +275,7 @@ public class ProjetosUtil {
 		lProjeto21.setCategoria("Pesquisa aplicada");
 		lProjeto21.setSituacao(Situacao.SUBMETIDO.codigo);
 		lProjeto21.setArea("COORDENADORIA CURSOS TECNICO EM AGROECOLOGIA (CAMPUS LAGES) (11.01.21.02.10)");
-		lProjeto21.setEmailCoordenador("jocleita.ferrareze@ifsc.edu.br|joferrareze@yahoo.com.br");
+		lProjeto21.setEmailCoordenador("jocleita.ferrareze@ifsc.edu.br | joferrareze@yahoo.com.br");
 		lProjeto21.setCoordenador("JOCLEITA PERUZZO FERRAREZE");
 		lProjeto21.setDataCadastro(DateUtil.criarData(10, 4, 2019));
 		listProjetos.add(lProjeto21);
@@ -287,7 +287,7 @@ public class ProjetosUtil {
 		lProjeto22.setCategoria("Pesquisa aplicada");
 		lProjeto22.setSituacao(Situacao.REPROVADO.codigo);
 		lProjeto22.setArea("COORDENADORIA CURSOS TECNICO EM AGROECOLOGIA (CAMPUS LAGES) (11.01.21.02.10)");
-		lProjeto22.setEmailCoordenador("jose.mecabo@ifsc.edu.br|josemecabo@yahoo.com.br");
+		lProjeto22.setEmailCoordenador("jose.mecabo@ifsc.edu.br | josemecabo@yahoo.com.br");
 		lProjeto22.setCoordenador("JOSE MECABO JUNIOR");
 		lProjeto22.setDataCadastro(DateUtil.criarData(9, 4, 2019));
 		listProjetos.add(lProjeto22);
@@ -311,7 +311,7 @@ public class ProjetosUtil {
 		lProjeto24.setCategoria("Pesquisa aplicada");
 		lProjeto24.setSituacao(Situacao.REPROVADO.codigo);
 		lProjeto24.setArea("COORDENADORIA DO CURSO TECNICO EM INFORMATICA PARA INTERNET (CAMPUS LAGES) (11.01.21.02.20)");
-		lProjeto24.setEmailCoordenador("juliano.goncalves@ifsc.edu.br|julianolg@gmail.com");
+		lProjeto24.setEmailCoordenador("juliano.goncalves@ifsc.edu.br | julianolg@gmail.com");
 		lProjeto24.setCoordenador("JULIANO LUCAS GONCALVES");
 		lProjeto24.setDataCadastro(DateUtil.criarData(10, 4, 2019));
 		listProjetos.add(lProjeto24);
@@ -323,7 +323,7 @@ public class ProjetosUtil {
 		lProjeto25.setCategoria("Pesquisa aplicada");
 		lProjeto25.setSituacao(Situacao.SUBMETIDO.codigo);
 		lProjeto25.setArea("COORDENADORIA DO CURSO SUPERIOR DE TECNOLOGIA EM GESTAO DO AGRONEGOCIO (CAMPUS LAGES) (11.01.21.02.14.02)");
-		lProjeto25.setEmailCoordenador("luciane.costa@ifsc.edu.br|lucianecosta.ifsc@gmail.com");
+		lProjeto25.setEmailCoordenador("luciane.costa@ifsc.edu.br | lucianecosta.ifsc@gmail.com");
 		lProjeto25.setCoordenador("LUCIANE COSTA DE OLIVEIRA");
 		lProjeto25.setDataCadastro(DateUtil.criarData(12, 10, 2019));
 		listProjetos.add(lProjeto25);
@@ -335,7 +335,7 @@ public class ProjetosUtil {
 		lProjeto26.setCategoria("Pesquisa aplicada");
 		lProjeto26.setSituacao(Situacao.SUBMETIDO.codigo);
 		lProjeto26.setArea("COORDENADORIA DO CURSO TECNICO EM ADMINISTRAÇÃO (CAMPUS LAGES) (11.01.21.02.19)");
-		lProjeto26.setEmailCoordenador("marisa.sanson@ifsc.edu.br|sanson.marisa@gmail.com");
+		lProjeto26.setEmailCoordenador("marisa.sanson@ifsc.edu.br | sanson.marisa@gmail.com");
 		lProjeto26.setCoordenador("MARISA SANTOS SANSON");
 		lProjeto26.setDataCadastro(DateUtil.criarData(13, 11, 2019));
 		listProjetos.add(lProjeto26);
@@ -347,7 +347,7 @@ public class ProjetosUtil {
 		lProjeto27.setCategoria("Pesquisa aplicada");
 		lProjeto27.setSituacao(Situacao.SUBMETIDO.codigo);
 		lProjeto27.setArea("COORDENADORIA DO CURSO SUPERIOR EM ENGENHARIA MECÂNICA (CAMPUS LAGES) (11.01.21.02.18)");
-		lProjeto27.setEmailCoordenador("matheus.fontanelle@ifsc.edu.br|matheus.ifsc@gmail.com");
+		lProjeto27.setEmailCoordenador("matheus.fontanelle@ifsc.edu.br | matheus.ifsc@gmail.com");
 		lProjeto27.setCoordenador("MATHEUS FONTANELLE PEREIRA");
 		lProjeto27.setDataCadastro(DateUtil.criarData(04, 11, 2019));
 		listProjetos.add(lProjeto27);
@@ -369,7 +369,7 @@ public class ProjetosUtil {
 		lProjeto29.setTitulo("Desenvolvimento de nanopartículas de prata para aplicação em enxertos ósseos de uso odontológico");
 		lProjeto29.setTipo("Projeto Interno por Editais (Projeto Novo)");
 		lProjeto29.setCategoria("Pesquisa aplicada");
-		lProjeto29.setSituacao(Situacao.EM_EXECUÇÃO.codigo);
+		lProjeto29.setSituacao(Situacao.EM_EXECUCAO.codigo);
 		lProjeto29.setArea("DEPARTAMENTO DE ENSINO, PESQUISA E EXTENSÃO (CAMPUS LAGES) (11.01.21.02)");
 		lProjeto29.setEmailCoordenador("michael.nunes@ifsc.edu.br");
 		lProjeto29.setCoordenador("MICHAEL RAMOS NUNES");
@@ -393,7 +393,7 @@ public class ProjetosUtil {
 		lProjeto31.setTitulo("10 anos de IFSC em Lages: a história do câmpus");
 		lProjeto31.setTipo("Projeto Interno por Editais (Projeto Novo)");
 		lProjeto31.setCategoria("Pesquisa aplicada");
-		lProjeto31.setSituacao(Situacao.EM_EXECUÇÃO.codigo);
+		lProjeto31.setSituacao(Situacao.EM_EXECUCAO.codigo);
 		lProjeto31.setArea("COORDENADORIA DO CURSO TECNICO EM INFORMATICA PARA INTERNET (CAMPUS LAGES) (11.01.21.02.20)");
 		lProjeto31.setEmailCoordenador("paula.jesus@ifsc.edu.br");
 		lProjeto31.setCoordenador("PAULA CLARICE SANTOS GRAZZIOTIN DE JESUS");
@@ -405,7 +405,7 @@ public class ProjetosUtil {
 		lProjeto32.setTitulo("Biomassa e composição química do óleo essencial de Lavanda (Lavandula angustifolia Mill.) sob diferentes fontes de adubação");
 		lProjeto32.setTipo("Projeto Interno por Editais (Projeto Novo)");
 		lProjeto32.setCategoria("Pesquisa aplicada");
-		lProjeto32.setSituacao(Situacao.EM_EXECUÇÃO.codigo);
+		lProjeto32.setSituacao(Situacao.EM_EXECUCAO.codigo);
 		lProjeto32.setArea("COORDENADORIA CURSOS TECNICO EM AGROECOLOGIA (CAMPUS LAGES) (11.01.21.02.10)");
 		lProjeto32.setEmailCoordenador("paula.iaschitzki@ifsc.edu.br");
 		lProjeto32.setCoordenador("PAULA IASCHITZKI FERREIRA CIPRIANI");
